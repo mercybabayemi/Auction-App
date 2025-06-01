8f1cec45327717577786515bf1de2216f1e10f66
Mercy Babayemi <mercyjanet013@gmail.com>
2025-06-01 01:32:47 +0100

    project structure completed and index base file added with styling

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # return ("helloWorld")
     return render_template("index.html")
+
+@app.route('/base')
+def base():
+    # return ("helloWorld")
+    return render_template("base.html")
+
+@app.route('/auction')
+def auction():
+    return render_template("auction.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
